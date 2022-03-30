585e5141c10e5e053482bab66f5f02fa4f2314ea
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-03-30 09:38:43 +0200

    separación

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 console.log("holamundo");
+
 console.log("hola");
+
 console.log("rodri");
+
 console.log("Mario");
+
 console.log("Ceci");
+
 console.log("Guzman");
-console.log("Fabio");
-console.log("Sonia");
-console.log("Alberto");
-console.log("otro commit");
 console.log("Una linea mas");
+
+console.log("Fabio");
+
+console.log("Sonia");
+
+console.log("Alberto");
+
+console.log("otro commit");
