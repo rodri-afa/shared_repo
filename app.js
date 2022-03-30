9cf9087a1e5a5973661d83587e81e045817eca70
AdanPomboTorres <adanpombotorres@gmail.com>
2022-03-30 09:38:46 +0200

    mensaje

--- app.js
+++ app.js
@@ -7,8 +7,5 @@ console.log("Guzman");
 console.log("Fabio");
 console.log("Sonia");
 console.log("Alberto");
-<<<<<<< HEAD
-=======
 console.log("otro commit");
 console.log("Una linea mas");
->>>>>>> a261dfcb3e7786ad782db55c90f59675c3e30472
